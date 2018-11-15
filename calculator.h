a27d6b74a2e65fb679438e8bc7166bd1a179f235
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:43:25 +0200

    conflicts

--- calculator.h
+++ calculator.h
@@ -3,15 +3,9 @@
 
 class Calculator
 {
-<<<<<<< HEAD
     public:
-        int Add (double, double);
-        int Sub (double, double);
-=======
-	public:
-		int Add (double, double);
-		int Sub (double, double);
->>>>>>> 4ad40a3b4572deee4efc716e7bd6e6e4fda46dcf
+        int Add (int, int);
+        int Mul (double, double);
 };
 
 #endif//CALCULATOR_H
