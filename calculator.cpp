b4d15017d874773cc3ff21633cb3b2e44e0da170
Maksym <maksymgushcha20@gmail.com>
2024-09-29 16:18:35 +0300

    Added div operation and updated Add method

--- calculator.cpp
+++ calculator.cpp
@@ -1,29 +1,24 @@
 #include "calculator.h"
+#include <stdexcept>
 
-int Calculator::Add (double a, double b)
+double Calculator::Add(double a, double b) 
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
     return a + b;
-=======
-	return a + b + 0.5;
->>>>>>> 4ad40a3 (fix truncation error)
-=======
-	return a + b + 0.5;
->>>>>>> 4ad40a3b4572deee4efc716e7bd6e6e4fda46dcf
 }
 
-int Calculator::Sub (double a, double b)
+double Calculator::Mul(double a, double b) 
 {
-	return Add (a, -b);
+    return a * b;
 }
 
-int Calculator::Mul (double a, double b)
+double Calculator::Div(double a, double b) 
 {
-    return a * b + 0.5;
-}
-
-int Calculator::Mul (double a, double b)
-{
-    return a * b + 0.5;
+    if (b != 0) 
+    {
+        return a / b;
+    } 
+    else 
+    {
+        throw std::invalid_argument("Division by zero is not allowed.");
+    }
 }
--- calculator.h
+++ calculator.h
@@ -4,8 +4,9 @@
 class Calculator
 {
     public:
-        int Add (int, int);
-        int Mul (double, double);
+        double Add(double, double);
+        double Mul(double, double);
+        double Div(double, double); 
 };
 
 #endif//CALCULATOR_H
